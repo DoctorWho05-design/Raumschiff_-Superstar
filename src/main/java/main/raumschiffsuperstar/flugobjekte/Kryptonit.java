bb37724b66d0820f60f4bbcfb763fd25dc847790
DoctorWho05 <niklas.buecker.nb@gmail.com>
2023-12-22 16:27:34 +0100

    Remove thro new

--- src/main/java/main/raumschiffsuperstar/flugobjekte/Kryptonit.java
+++ src/main/java/main/raumschiffsuperstar/flugobjekte/Kryptonit.java
@@ -5,16 +5,15 @@ import java.awt.Point;
 
 public class Kryptonit extends UFO{
 
+    
     @Override
     public void draw(Graphics g) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'draw'");
     }
 
     @Override
     public void move(Point aPosEnemy) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'move'");
     }
     
 }

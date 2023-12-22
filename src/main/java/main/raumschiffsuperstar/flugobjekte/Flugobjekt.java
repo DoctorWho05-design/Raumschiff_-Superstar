d7d2c3fe79f93cf25e8c6c39b090cbb8a8f7f0ef
DoctorWho05 <niklas.buecker.nb@gmail.com>
2023-12-22 16:26:47 +0100

    Update Flugobjekt.java

--- src/main/java/main/raumschiffsuperstar/flugobjekte/Flugobjekt.java
+++ src/main/java/main/raumschiffsuperstar/flugobjekte/Flugobjekt.java
@@ -1,16 +1,22 @@
 package main.raumschiffsuperstar.flugobjekte;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Point;
 
 abstract class Flugobjekt {
+    //* Parameter
     protected Point aPosObjekt;
+    protected Dimension aSizeObj;
 
+    //* Constructor
     public Flugobjekt() {}
     
+    //* Abstract Methods 
     public abstract void draw(Graphics g);
-
     public abstract void move(Point aPosEnemy);
 
+    //* Getter
     public Point getPosObjekt() {return aPosObjekt;}
+    public Dimension getSizeObj() {return aSizeObj;}
 }

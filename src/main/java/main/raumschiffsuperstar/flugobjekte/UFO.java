f9e157fa31f1069a0450c0fd0d55ba99e57f5710
DoctorWho05 <niklas.buecker.nb@gmail.com>
2023-12-22 16:24:49 +0100

    Update UFO.java

--- src/main/java/main/raumschiffsuperstar/flugobjekte/UFO.java
+++ src/main/java/main/raumschiffsuperstar/flugobjekte/UFO.java
@@ -1,16 +1,20 @@
 package main.raumschiffsuperstar.flugobjekte;
 
 public abstract class UFO extends Flugobjekt{
+
+    //* Parameter
     private boolean aInGame;
     protected int aSpeed;
 
+    //* Method Used in timerTick() to check if the Object Hit the Spaceship
     public Boolean checkHitShip() {
         // TODO code Check if Object hit Ship
-        
         return false;
     }
 
+    //* Getter
     public Boolean getInGame() {return aInGame;}
 
+    //* Setter
     public void setInGame(boolean aInGame) {this.aInGame = aInGame;}
 }

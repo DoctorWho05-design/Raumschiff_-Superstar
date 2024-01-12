bb503a048f08f4198d29e5fd1bf634c04462f03b
DoctorWho05 <niklas.buecker.nb@gmail.com>
2024-01-12 15:26:50 +0100

    Combine Methods

--- src/main/java/main/raumschiffsuperstar/flugobjekte/Kryptonit.java
+++ src/main/java/main/raumschiffsuperstar/flugobjekte/Kryptonit.java
@@ -1,28 +1,10 @@
 package main.raumschiffsuperstar.flugobjekte;
 
-import java.awt.Graphics;
-import java.awt.Point;
-
 import main.raumschiffsuperstar.GameField;
 
 public class Kryptonit extends UFO{
 
     public Kryptonit(GameField aGameField) {
         super("./public/Kryptonit.png", aGameField);
-    }
-    
-    @Override
-    public void draw(Graphics g, GameField aGameField) {
-        // TODO Auto-generated method stub
-    }
-
-    @Override
-    public void move(Point aPosEnemy) {
-        // TODO Auto-generated method stub
-    }
-    
-    @Override
-    public void updateRank(){
-        
     }
 }
--- src/main/java/main/raumschiffsuperstar/flugobjekte/Superstar.java
+++ src/main/java/main/raumschiffsuperstar/flugobjekte/Superstar.java
@@ -1,7 +1,5 @@
 package main.raumschiffsuperstar.flugobjekte;
 
-import java.awt.Graphics;
-import java.awt.Point;
 
 import main.raumschiffsuperstar.GameField;
 
@@ -9,22 +7,5 @@ public class Superstar extends UFO{
 
     public Superstar(GameField aGameField) {
         super("./public/Superstar.png", aGameField);
-        aSpeed = random.nextInt(10);
-    }
-
-    @Override
-    public void draw(Graphics g, GameField aGameField) {
-        g.drawImage(aStartFieldImage, this.aPosObjekt.x, aPosObjekt.y, SIZE.width, SIZE.height, aGameField);
-    }
-
-    @Override
-    public void move(Point aPosEnemy) {
-        aPosObjekt.y += aSpeed;
-        System.out.println(aPosObjekt);
-    }
-    
-    @Override
-    public void updateRank(){
-        aSpeed *= 2;
     }
 }
